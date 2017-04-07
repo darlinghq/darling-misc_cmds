9e03c766998263c699ebaa836bb6f3d06c8fb407
Andrew Hyatt <ahyattdev@icloud.com>
2017-04-06 19:33:09 -0700

    Add build script and generated sources

---
+++ gen/calendar_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char __calendarVersionString[];
+ extern const double __calendarVersionNumber;
+
+ const unsigned char __calendarVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:calendar  PROJECT:misc_cmds-" "\n";
+ const double __calendarVersionNumber __attribute__ ((used)) = (double)0.;
---
+++ gen/units_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char __unitsVersionString[];
+ extern const double __unitsVersionNumber;
+
+ const unsigned char __unitsVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:units  PROJECT:misc_cmds-" "\n";
+ const double __unitsVersionNumber __attribute__ ((used)) = (double)0.;
