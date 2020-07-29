85b24ec0e2625d75e7ee75b597b9134a49d18b1f
Ariel Abreu <facekapow@outlook.com>
2020-07-29 12:02:42 -0400

    Darling build of misc_cmds-34

---
+++ gen/cal_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char __calVersionString[];
+ extern const double __calVersionNumber;
+
+ const unsigned char __calVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:cal  PROJECT:misc_cmds-" "\n";
+ const double __calVersionNumber __attribute__ ((used)) = (double)0.;
